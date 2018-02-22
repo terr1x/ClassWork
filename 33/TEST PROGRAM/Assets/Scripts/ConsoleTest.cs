e9462dedeb7bf20dee8c343a918fc7a98a11f9bb
TERcrash <TERcrash@users.noreply.github.com>
2018-02-22 14:32:54 +0300

    Вота она

---
+++ 33/TEST PROGRAM/Assets/Scripts/ConsoleTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsoleTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    }
+
+    // Update is called once per frame
+    void Update () {
+        print("x = " + transform.position.x + " y = " + transform.position.y + " z = "+transform.position.z);
+	}
+}

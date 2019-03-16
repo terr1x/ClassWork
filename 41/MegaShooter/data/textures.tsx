7012ea84a90e482b51d9a1203853d32af1b8e2d2
TERcrash <TERcrash@users.noreply.github.com>
2019-03-16 19:36:12 +0300

    пуш

---
+++ 41/MegaShooter/data/textures.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="textures" tilewidth="57" tileheight="45" tilecount="8" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="19">
+  <image width="32" height="32" source="sky.png"/>
+ </tile>
+ <tile id="20">
+  <image width="32" height="37" source="soldier.png"/>
+ </tile>
+ <tile id="21">
+  <image width="57" height="44" source="движение.png"/>
+ </tile>
+ <tile id="22">
+  <image width="32" height="32" source="Прозрачный тайл.png"/>
+ </tile>
+ <tile id="23">
+  <image width="57" height="44" source="стояние.png"/>
+ </tile>
+ <tile id="24">
+  <image width="36" height="45" source="enemy.png"/>
+ </tile>
+ <tile id="25">
+  <image width="32" height="32" source="ground.png"/>
+ </tile>
+ <tile id="26">
+  <image width="32" height="32" source="mine.png"/>
+ </tile>
+</tileset>

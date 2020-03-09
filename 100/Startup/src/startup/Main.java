04072f47a463513eac9532dda6fccb09551cdb26
TERcrash <TERcrash@users.noreply.github.com>
2020-03-09 13:13:29 +0300

    доделали игру про старт ап

--- 100/Startup/src/startup/Main.java
+++ 100/Startup/src/startup/Main.java
@@ -1,29 +1,31 @@
 package startup;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Random;
 
 public class Main {
 
+
     public static void main(String[] args) {
+        try {
+            clearFile();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
         Programmer[] programmers = new Programmer[(int) (Math.random() * 5) + 6];
+
+        Manager manager = new Manager(generateName(5, 10), programmers);
+
         for (int i = 0; i < programmers.length; i++) {
             programmers[i] = new Programmer(generateName(4, 8), generateLanguage());
         }
-        Manager manager = new Manager(generateName(5, 10), programmers);
-
-
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("resources/startup.info"));
-            writer.write(manager.toString() + "\n");
-            for (int i = 0; i < programmers.length; i++) {
-                writer.write(programmers[i].toString() + "\n");
-            }
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        writeIntoDoc(manager.toString());
+        for (int i = 0; i < programmers.length; i++) {
+            writeIntoDoc(programmers[i].toString());
         }
 
         int loc=manager.doWork();
@@ -34,6 +36,17 @@ public class Main {
             } catch (StartupException e) {
                 break;
             }
+        }
+        int work=manager.doWork();
+        if(work-loc>0) {
+            writeIntoDoc("Програмисты стали писать на "+(work-loc)+" больше кода");
+        }else{
+            writeIntoDoc("Програмисты стали писать на "+Math.abs(work-loc)+" меньше кода");
+        }
+        writeIntoDoc("Менеджер и его команда после обучения:");
+        writeIntoDoc(manager.toString());
+        for (int i = 0; i < programmers.length; i++) {
+            writeIntoDoc(programmers[i].toString());
         }
     }
 
@@ -57,6 +70,23 @@ public class Main {
             return "C++";
         } else {
             return "C#";
+        }
+    }
+
+    private static void writeIntoDoc(String text){
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("resources/startup.info",true));
+            writer.write(text+"\n");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void clearFile() throws IOException {
+        File file= new File("resources/startup.info");
+        if(file.delete()){
+            file.createNewFile();
         }
     }
 }
